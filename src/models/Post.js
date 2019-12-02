b4313461922d6075c7da40cfc375a7a97f9e3ec0
Vinicius Basniak <vinicius.basniak@gmail.com>
2019-12-02 20:46:03 -0300

    post_image array

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,42 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
 
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
+  post_date: {
+    type: Date,
+    default: Date.now
+  },
 
-    post_date: {
-        type: Date,
-        default: Date.now,
-    },
+  post_image: [{ type: String, required: true }],
 
-    post_loc: {
-        type: String,
-        required: true,
-    },
+  post_loc: {
+    type: String,
+    required: true
+  },
 
-    post_text: {
-        type: String,
-        required: true,
-    },
+  post_text: {
+    type: String,
+    required: true
+  },
 
-    post_like: {
-        type: Number,
-        required: true,
-    },
+  post_like: {
+    type: Number,
+    required: true
+  },
 
-    post_solved: {
-        type: Number,
-        required: true,
-    },
+  post_solved: {
+    type: Number,
+    required: true
+  },
 
-    post_com: {
-        type: Number,
-        required: true,
-    },
+  post_com: {
+    type: Number,
+    required: true
+  }
 });
 
-mongoose.model('Post', PostSchema);
+mongoose.model("Post", PostSchema);
