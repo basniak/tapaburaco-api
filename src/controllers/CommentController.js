c813b045f38542316ebcee66a8a87c6832551f85
Vinicius Basniak <vinicius.basniak@gmail.com>
2019-12-05 17:52:41 -0300

    commentarios feitos do jeito certo

--- src/controllers/CommentController.js
+++ src/controllers/CommentController.js
@@ -4,15 +4,21 @@ const Comment = mongoose.model("Comment");
 
 module.exports = {
   async getallcommentsbypostid(req, res) {
-    const comments = await Comment.findOne(req.post_id);
+    const comments = await Comment.findOne(res.post_id);
 
     return res.json(comments);
   },
 
-  async createcomment(req, res) {
-    const comment = await Comment.create(res.body);
-
-    return res.json(comment);
+  async createcomment(req, res, next) {
+    // console.log(req.body)
+    return Comment.create(req.body, (err, data) => {
+      if (err) {
+        console.log(err)
+        next(err)
+      } else {
+        res.json(data)
+      }
+    });
   },
 
   async updatecommentbyid(req, res) {
